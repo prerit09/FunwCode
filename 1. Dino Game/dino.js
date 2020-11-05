469c97125574ea7b1b0534bb46581022519a21c4
prerit09 <preritj.27@gmail.com>
2020-11-06 01:35:30 +0530

    added dino

---
+++ 1. Dino Game/dino.js
@@ -0,0 +1,13 @@
+// To open console press Ctrl+Shift+I 
+
+// Access Variables and Functions
+Runner.instance_
+
+// Increase Dino Speed
+Runner.instance_.setSpeed(150)
+
+// Set Jump Velocity
+Runner.instance_.tRex.setJumpVelocity(100)
+
+// Dino God Mode
+Runner.instance_.gameOver = () => { console.log("I AM GOD!!!") }
