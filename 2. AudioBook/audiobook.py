e0e9f015a7e34122261e4c7f32511a67738ff74b
prerit09 <preritj.27@gmail.com>
2020-11-06 02:22:24 +0530

    added function

--- 2. AudioBook/audiobook.py
+++ 2. AudioBook/audiobook.py
@@ -1,18 +1,48 @@
 import pyttsx3
 import PyPDF2
+import time
 
-book=open('Book.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(book)
+book_path = "Book.pdf"
+gender = 1
+words_per_minute = 150
+first_page = 14
 
-pages = pdfReader.numPages
-print(pages) 
+def read_book(book_path, gender, words_per_minute, first_page):
 
+    # Opens the book to be read
+    book=open(book_path, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(book)
 
-speaker = pyttsx3.init()
+    # Initializes the narrator
+    narrator = pyttsx3.init()
 
-page = pdfReader.getPage(14)
-text = page.extractText()
+    # Sets the voice of the narrator 
+    narrator.setProperty('voice',narrator.getProperty('voices')[gender].id)
 
-speaker.say(text)
+    # Sets the narration speed
+    narrator.setProperty('rate', words_per_minute) 
 
-speaker.runAndWait()
+    # Count and print total number of pages in the book
+    total_pages = pdfReader.numPages
+    print(total_pages) 
+    
+    continue_reading = "y"
+    
+    for num in range(first_page, total_pages):
+        while continue_reading in ["y","Y"]:
+            print("Reading Page Number : " + str(num))
+            # Fetches text from the current page
+            page = pdfReader.getPage(num)
+            text = page.extractText()
+
+            # Narrates the text
+            narrator.say("Reading Page Number "+ str(num))
+            time.sleep(2)
+            narrator.say(text)
+            narrator.runAndWait()
+
+            continue_reading = input("Do you want to continue reading (y/n) ? ")
+
+    book.close()
+
+read_book(book_path, gender, words_per_minute, first_page)
