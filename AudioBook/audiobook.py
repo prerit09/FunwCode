9405d032aef418526f8f47a5a398400988b05841
prerit09 <preritj.27@gmail.com>
2020-11-06 01:21:00 +0530

    "added audiobook files"

---
+++ AudioBook/audiobook.py
@@ -0,0 +1,17 @@
+import pyttsx3
+import PyPDF2
+
+book=open('Book.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+
+pages = pdfReader.numPages
+print(pages)
+
+speaker = pyttsx3.init()
+
+page = pdfReader.getPage(14)
+text = page.extractText()
+
+speaker.say(text)
+
+speaker.runAndWait()
